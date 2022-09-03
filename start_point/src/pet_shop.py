c44ad7e63c2c609f591a1269f385c22bfddf7cf6
Colin Cameron <ccameron2020@gmail.com>
2022-09-03 12:08:54 +0100

    optional and first extention done

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -23,10 +23,11 @@ def get_stock_count(list):
 
 def get_pets_by_breed(list, breed):
     found_pets = []
-    for pet in list['pets']:
+    for pet in list['pets']:     # LIST INSIDE A DICT
         if pet['breed'] == breed:
             found_pets.append(pet)
     return found_pets
+
 
 def find_pet_by_name(list, name):
     for pet in list['pets']:
@@ -36,12 +37,12 @@ def find_pet_by_name(list, name):
 def remove_pet_by_name(list, name):
     for pet in list['pets']:
         if pet['name'] == name:
-            list['pets'].remove(pet)
+            list['pets'].remove(pet) # Trust in code, not Vsc, white does not mean not working
 
 
 
 def add_pet_to_stock(list, name):
-    return list['pets'].append(name)
+    return list['pets'].append(name) # Same here
 
 def get_customer_cash(list):
     return list['cash']
@@ -55,6 +56,33 @@ def get_customer_pet_count(list):
 
 def add_pet_to_customer(cust, new_pet):
         cust["pets"].append(new_pet)
-#You should commit when you get to here
+
+#You should commit when you get to here if you havent already!
 ##########################################################
 # optional below        
+
+def customer_can_afford_pet(cust, new_pet):
+    if cust['cash'] >= new_pet['price']:
+        return True
+    else:
+        return False
+#This seemed too easy for all 3 tests to run
+#Check with instructors?
+
+#remove pet form petshop, 3
+#add pet to cust list['pets'] 1
+#remove price of pet from customers['cash'] 4
+#add money to cc_pet_shop['total_cash] 2
+
+
+def sell_pet_to_customer(list, buy_pet, cust):
+    for pet in list['pets']:
+        if pet == buy_pet:
+            cust['pets'].append(pet)
+            list['pets'].remove(pet)
+    list['admin']['total_cash'] += buy_pet['price']
+    cust['cash'] -= buy_pet['price']
+    list["admin"]['pets_sold'] += 1
+
+
+def      
